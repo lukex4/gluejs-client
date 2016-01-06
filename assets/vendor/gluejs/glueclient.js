e0425855887fdfe8cacb0c92dfe9bd4fe8c9b66c
Luke NC <lukenicholascohen@gmail.com>
2016-01-06 17:44:56 +0000

    Fixed isRelName() bug

--- assets/vendor/gluejs/glueclient.js
+++ assets/vendor/gluejs/glueclient.js
@@ -917,7 +917,7 @@ var GlueClient = function(socketObj, generalScope){
   var isRelName = function(nameToCheck) {
     cons && console.log("isRelName", nameToCheck);
 
-    if (typeof nameToCheck === 'string' && /^[a-zA-Z0-9-_]+$/.test(nameToCheck) === false) {
+    if (typeof nameToCheck === 'string' && /^([a-zA-Z0-9_-]+)$/i.test(nameToCheck) === true) {
       return true;
     } else {
       return false;
